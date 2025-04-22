b6e76ac02fa908255dc886beae58a412b46547f4
mdtanvir200 <mdtanvir.ahmed@teamgrid.io>
2025-04-22 17:45:40 +0530

    added

--- backend/models/representativeDataSchema.js
+++ backend/models/representativeDataSchema.js
@@ -1,12 +1,23 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+
 const RepresentativeDataSchema = new mongoose.Schema({
     houseId: { type: mongoose.Schema.Types.ObjectId, ref: 'representatives' },
     termId: { type: mongoose.Schema.Types.ObjectId, ref: 'terms' },
     currentTerm: Boolean,
     summary: String,
     rating: String,
-    votesScore: [{ voteId: mongoose.Schema.Types.ObjectId, score: String }],
-    activitiesScore: [{ activityId: mongoose.Schema.Types.ObjectId, score: String }]
-  });
-  
-  module.exports=mongoose.model('representative_datas', RepresentativeDataSchema);
+    votesScore: [
+        { 
+            voteId: { type: mongoose.Schema.Types.ObjectId, ref: 'votes' }, 
+            score: String 
+        }
+    ],
+    activitiesScore: [
+        { 
+            activityId: { type: mongoose.Schema.Types.ObjectId, ref: 'activities' }, 
+            score: String 
+        }
+    ]
+});
+
+module.exports = mongoose.model('representative_datas', RepresentativeDataSchema);
