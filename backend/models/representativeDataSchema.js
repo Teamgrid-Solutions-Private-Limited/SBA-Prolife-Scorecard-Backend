8e276f606bf79c9fe36d7457c22f1b1363acec8e
Sk Saruk Ali <sksaruk.ali@teamgrid.io>
2025-08-28 17:18:11 +0530

    changed repdata schema add index

--- backend/models/representativeDataSchema.js
+++ backend/models/representativeDataSchema.js
@@ -21,7 +21,7 @@ const RepresentativeDataSchema = new mongoose.Schema({
 },{timestamps: true});
 // ✅ Ensure only ONE currentTerm per houseId (representative)
 RepresentativeDataSchema.index(
-  { houseId: 1, currentTerm: 1 },
+  { houseId: 1, termId: 1, currentTerm: 1 },
   { unique: true, partialFilterExpression: { currentTerm: true } }
 );
 module.exports = mongoose.model('representative_datas', RepresentativeDataSchema);
